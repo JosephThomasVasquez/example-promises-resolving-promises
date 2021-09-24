5ad11ca82d16b62ad6aff79ba60fbf6c8577d09a
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-09-23 18:40:47 -0700

    Added Promises Assignment solution

--- src/main.js
+++ src/main.js
@@ -1,10 +1,51 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-const question = "Will the weather be nice today?";
-const promise = tell(question);
-promise
-  .then((fortune) => {
-    console.log(question);
-    console.log(fortune);
-  })
-  .catch(console.log);
+let question = "When will the PS5 be restocked?";
+
+// const promise = tell(question);
+
+function getFortune(question) {
+  return tell(question)
+    .then((fortune) => {
+      return [`Your question was: ${question}`, `Your fortune is: ${fortune}`];
+    })
+    .catch((error) => {
+      return `There was an error: ${error}`;
+    });
+}
+
+getFortune("Will I ace my job interview?");
+
+function fullSession(question) {
+  return welcome().then((welcomeMsg) => {
+    const result = [];
+    result.push(welcomeMsg);
+
+    tell(question)
+      .then((fortune) => {
+        result.push(
+          `Your question was: ${question}`,
+          `Your fortune is: ${fortune}`
+        );
+        return result;
+      })
+      .then(() => {
+        goodbye().then((response) => {
+          result.push(response);
+          console.log(result);
+          return result;
+        });
+      })
+      .catch((error) => {
+        result.push(`There was an error: ${error}`);
+
+        goodbye().then((response) => {
+          result.push(response);
+          console.log(result);
+          return result;
+        });
+      });
+  });
+}
+
+fullSession();
